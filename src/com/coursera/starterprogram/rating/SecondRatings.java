78c58d0df835822339360a62ec9e6401711ccebc
asa murphy <asa.g.murphy75@gmail.com>
2024-04-19 13:50:51 -0400

    starting step two with SecondRatings.java

---
+++ src/com/coursera/starterprogram/rating/SecondRatings.java
@@ -0,0 +1,28 @@
+package com.coursera.starterprogram.rating;
+/**
+ * Write a description of SecondRatings here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import com.coursera.starterprogram.movie.Movie;
+import com.coursera.starterprogram.rater.Rater;
+
+import java.util.*;
+
+public class SecondRatings {
+    private ArrayList<Movie> myMovies;
+    private ArrayList<Rater> myRaters;
+    
+    public SecondRatings() {
+        // default constructor
+        this("ratedmoviesfull.csv", "ratings.csv");
+    }
+    public SecondRatings(String movieFile, String ratingsFile) {
+        FirstRatings fr = new FirstRatings();
+        myMovies = fr.loadMovies(movieFile);
+        myRaters = fr.loadRaters(ratingsFile);
+    }
+    
+}

46d8a01d9f81887802e577c542ed9b890cebf955
asa murphy <asa.g.murphy75@gmail.com>
2024-04-27 14:01:08 -0400

    Project Completed

--- src/com/coursera/capstone/recommender/RecommendationRunner.java
+++ src/com/coursera/capstone/recommender/RecommendationRunner.java
@@ -1,54 +1,115 @@
 package com.coursera.capstone.recommender;
 
 import com.coursera.capstone.database.MovieDatabase;
+import com.coursera.capstone.database.RaterDatabase;
+import com.coursera.capstone.filter.AllFilters;
+import com.coursera.capstone.filter.MinutesFilter;
 import com.coursera.capstone.filter.TrueFilter;
+import com.coursera.capstone.filter.YearAfterFilter;
 import com.coursera.capstone.rating.FourthRatings;
 import com.coursera.capstone.rating.Rating;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RecommendationRunner implements Recommender{
     @Override
     public ArrayList<String> getItemsToRate() {
-        ArrayList<String> movieIDs = MovieDatabase.filterBy(new TrueFilter());
-        ArrayList<String> moviesToRate = new ArrayList<>();
-        for(int i = 0; i < 15; i++) {
-            moviesToRate.add(movieIDs.get(i));
+        MovieDatabase.initialize("ratedmoviesfull.csv");
+        AllFilters filters = new AllFilters();
+        YearAfterFilter yearAfterFilter = new YearAfterFilter(2005);
+        MinutesFilter minutesFilter = new MinutesFilter(100, 150);
+        filters.addFilter(yearAfterFilter);
+        filters.addFilter(minutesFilter);
+        ArrayList<String> movieIds = MovieDatabase.filterBy(filters);
+        ArrayList<String> selectedMovieIds = new ArrayList<>();
+        int numOfMovie = 0 ;
+        Random rand = new Random();
+        while (numOfMovie < 30) {
+            int selectedMovieIdIndex = rand.nextInt(movieIds.size());
+            String selectedMovieId = movieIds.get(selectedMovieIdIndex);
+            selectedMovieIds.add(selectedMovieId);
+            numOfMovie += 1 ;
         }
-        return moviesToRate;
+        return selectedMovieIds ;
     }
 
     @Override
     public void printRecommendationsFor(String webRaterID) {
-        FourthRatings fourthRatings = new FourthRatings();
-        TrueFilter trueFilter = new TrueFilter();
-        int numSimilarRaters = 20;
-        int minimalRaters = 5;
-        int count = 0;
+        FourthRatings forthRating = new FourthRatings();
+        RaterDatabase.initialize("ratings.csv");
+        ArrayList<Rating> movieRatings = forthRating.getSimilarRatings(webRaterID, 20, 3);
+        String openHtml =
+                """ 
+                        <style>
+                         .checked {
+                           color: orange;
+                         }
+                         
+                         table {
+                           font-family: arial, sans-serif;
+                           border-collapse: collapse;
+                           width: 100%;
+                         }
+                         
+                         td, th {
+                           border: 1px solid #dddddd;
+                           text-align: left;
+                           padding: 8px;
+                         }
+                         
+                         tr:nth-child(even) {
+                           background-color: #dddddd;
+                         }
+                        </style>
+                        <table>
+                        <tr>
+                                <th>Title</th>
+                                <th>Genres</th>
+                                <th>Year</th>
+                                <th>Minutes</th>
+                                <th>Ratings</th>
+                        </tr>
+                                        
+                                        
+                        """;
 
-        ArrayList<Rating> ratingList = fourthRatings.getSimilarRatingsByFilter(
-                webRaterID, numSimilarRaters,  minimalRaters, trueFilter);
-
-        String html_head = "<html><head><title>Movie result</title>"+
-                "<style>" +
-                "</style></head><body><table>";
-
-        String result = "";
-        for (Rating rating : ratingList) {
-            result += "<tr>"  +
-                    "<td>" + MovieDatabase.getTitle(rating.getItem()) + "</td>" +
-                    "<td>" + MovieDatabase.getGenres(rating.getItem()) + "</td>" +
-                    "<td>" + MovieDatabase.getYear(rating.getItem()) + "</td>" +
-                    "<td>" + MovieDatabase.getMinutes(rating.getItem()) + "</td>" +
-                    "</tr>";
-            count++;
-            if (count == 10) {
-                break;
+        for (Rating movieRating : movieRatings) {
+            String movieId = movieRating.getItem();
+            int movieWA = (int) movieRating.getValue();
+            String title = MovieDatabase.getTitle(movieId);
+            String genres = MovieDatabase.getGenres(movieId);
+            int year = MovieDatabase.getYear(movieId);
+            int minutes = MovieDatabase.getMinutes(movieId);
+            String poster = MovieDatabase.getPoster(movieId);
+            String checkedStar = """
+                    <span class="checked">&spades;
+                    </span>""";
+            String uncheckedStar = """
+                    <span >&spades;</span>
+                    """;
+            String star = "" ;
+            for (int i = 0; i < 10 ; i++) {
+                if (i < movieWA) {
+                    star += checkedStar;
+                } else {
+                    star += uncheckedStar;
+                }
             }
-        }
-        String html_end = "</table></body></html>";
-        System.out.println(html_head + result + html_end);
 
+            String tableRow = String.format("""
+                    <tr>
+                        <td><a href="%s">%s</a></td>
+                        <td>%s</td>
+                        <td>%d</td>
+                        <td>%d</td>
+                        <td>%s</td>
+                    </tr>
+                    """,poster, title, genres, year, minutes, star);
+            openHtml += tableRow;
+        }
+        openHtml = openHtml + "</table>";
+        System.out.println(openHtml);
     }
 
 }
